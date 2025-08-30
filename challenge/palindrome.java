24da90f54cd7cbf429e09bbe9fc4fc7206b8c8b7
Luigibarte4563 <luigibarte1@gmail.com>
2025-08-30 21:43:38 +0800

    Add files via upload

--- challenge/palindrome.java
+++ challenge/palindrome.java
@@ -1,24 +1,21 @@
-package challenge;
-
 import java.util.Scanner;
 
-public class palindrome {
-    public static void main(String[]args) {
-
-        Scanner s = new Scanner(System.in);
-
-        System.out.print("Enter a word: ");
-        String word = s.nextLine();
-
-        String reverse = "";
-        for(int a = word.length() - 1; a >= 0; a--){
-            reverse = reverse + word.charAt(a);
-        }
-
-        if(word.equals(reverse)) {
-            System.out.println("Yes, its palindrome");
-        }else {
-            System.out.println("No, its not palindrome");
-        }
-    }    
+public class Main {
+	public static void main(String[] args) {
+		Scanner s = new Scanner(System.in);
+		
+		System.out.print("enter a word: ");
+		String word = s.nextLine();
+		
+		String reverse = "";
+		for(int a = word.length() - 1; a >= 0; a--) {
+			reverse = reverse + word.charAt(a);	
+		}
+		
+		if(word.equals(reverse)) {
+			System.out.println("Yes, its palindrome");
+		}else {
+			System.out.println("No, its not Palindrome");
+		}
+	}
 }
