7c635863ce500c38c9a9ee09319b0e017ae8f6d6
Luigi Barte <luigibarte1@gmail.com>
2025-10-17 21:55:43 +0800

    Add files via upload

--- challenge/factorial.java
+++ challenge/factorial.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
 
-public class Main {
-	public static void main(String[] args) {
-		Scanner s = new Scanner(System.in);
-		
-		System.out.print("enter a factorial number: ");
-		int n = s.nextInt();
-		
-		int factorial = 1;
-		
-		for(int a = 1; a <= n; a++){
-			factorial *= a;	
-		}
-		
-		System.out.println("the factorial of " + " " + n + " " + "is " + factorial);
-	}
+class factorial {
+    public static void main(String[]args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.print("Enter a number: ");
+        int num = s.nextInt();
+
+        int factorial = 1;
+
+        for(int a = 1; a <= num; a++) {
+            factorial = factorial * a;
+        }
+
+        System.out.println("The factorial of " + num + " is " + factorial);
+    }
 }
